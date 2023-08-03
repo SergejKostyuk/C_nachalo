fcbe0a3ee0e3ced28bbd933665607171d1c508e1
Sergej <serega95kost@gmail.com>
2023-08-03 15:56:56 +0300

    Задача 07

---
+++ TASK07/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Вдедите трехзначное число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+if (chislo <= 0)
+{
+    chislo*=-1;
+}
+if (chislo >= 100 && chislo <= 999)
+{
+    int lastDigit = chislo%10;
+    Console.WriteLine("Последняя цифра:  " + lastDigit);  
+}
+else
+{
+    Console.WriteLine("Некорректное значение");
+}
