021671ea662a23f1f111ffaae9c8818e67f991dd
Sergej <serega95kost@gmail.com>
2023-08-04 15:01:28 +0300

    Задача 14

---
+++ TASK14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoDigit(num, 7, 23);
+Console.WriteLine(result?"Да":"Нет");
+
+bool MultiplicityTwoDigit(int numb, int numb1, int numb2)
+{
+    return numb % numb1 ==0 && numb % numb2 == 0;
+}
+
+
