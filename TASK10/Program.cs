93a4ac92c2182a33e807543b381bf396fcbb5be6
Sergej <serega95kost@gmail.com>
2023-08-04 20:54:35 +0300

    Задание 10

---
+++ TASK10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число, от 100 до 999 -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = ResultSecondNumber(number);
+Console.WriteLine(result);
+
+
+int ResultSecondNumber(int numb)
+{
+    return result = numb /10 % 10;
+
+}
