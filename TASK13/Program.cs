44bfbf56ab909e720cc768cfa538773eed3e3632
Sergej <serega95kost@gmail.com>
2023-08-06 21:30:33 +0300

    Задача 15

--- TASK13/Program.cs
+++ TASK13/Program.cs
@@ -4,3 +4,11 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Введите число, от 0 до n -> ");
+int numder = Convert.ToInt32(Console.ReadLine());
+
+
+int result = numder % 100;
+
+Console.WriteLine($"Третья цифра числа: {result}");
--- TASK15/Program.cs
+++ TASK15/Program.cs
@@ -4,3 +4,20 @@
 // // 6 -> да
 // // 7 -> да
 // // // 1 -> нет
+
+Console.WriteLine("Введите цифру, от 1 до 7 дня недели");
+string [] array = {"понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"};
+int num = Convert.ToInt32(Console.ReadLine()) - 1;
+
+if (num >= 0 && num <=7)
+{
+    bool dey = Dey (num);
+    Console.Write($"Сегодня {array[num]}, ");
+    Console.WriteLine(dey?"будний день":"выходной день");
+}
+else Console.WriteLine("Введите коректное число!");
+
+bool Dey(int numb)
+{
+    return (numb >=0 && numb <=4);
+}
