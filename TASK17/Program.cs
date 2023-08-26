dfd5000ac88ec10f7000fbc924298390950c7f95
Sergej <serega95kost@gmail.com>
2023-08-26 17:55:10 +0300

    Задача 17, исправлени

--- TASK17/Program.cs
+++ TASK17/Program.cs
@@ -6,8 +6,8 @@ int Quater(int x, int y)
 {
     if (x > 0 && y > 0) return 1;
     if (x < 0 && y > 0) return 2;
-    if (x > 0 && y < 0) return 3;
-    if (x < 0 && y < 0) return 4;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return 0;
 }
 
