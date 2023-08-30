7387de0dd03ba55731d9fe0da1764584e3b47442
Sergej <serega95kost@gmail.com>
2023-08-30 19:19:42 +0300

    Задача 27

--- TASK27/Program.cs
+++ TASK27/Program.cs
@@ -1,19 +1,38 @@
 ﻿// Напишите программу, которая принимает
 // на вход число и выдаёт сумму цифр в числе. 
 
-int SumNumbers (int numb)
+double SumNumbers (int numb)
 {
-    int sum=0;
-    while (numb > 0)
+    double sum=0;
+    int count=0;
+    int numbOTR = numb * -1; // создаеться переменная для решения с отрицательным числом
+    if (numb == 0)
     {
-        sum = sum + (numb %10);
-        numb = numb /10;
-    } return sum;
+        return 0;
+    } else if (numb >0)
+    {
+        while (numb > 0)
+        {
+            sum = sum + (numb %10);
+            numb = numb /10;
+        } 
+        return sum;
+    } else 
+    while (numbOTR > 10)
+    {
+        sum = sum + (numbOTR %10);
+        numbOTR = numbOTR /10;
+        count ++; 
+    } 
+    double res = Convert.ToInt32(Math.Pow(10, count)); // возведение в степень количества операций
+    int resul = (int) res; // перевод значения res из типа double в int
+    sum = sum + numb / resul; 
+    return sum;   
+    
 }
-
-
+  
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int result = SumNumbers(number);
+double result = SumNumbers(number);
 Console.WriteLine($"Сумма цифр числа {number} ----> {result}");
