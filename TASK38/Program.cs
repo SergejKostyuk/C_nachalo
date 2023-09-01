13abb952d885a3275be2afcf6057996fa586ca20
Sergej <serega95kost@gmail.com>
2023-09-01 19:54:10 +0300

    Задача 38

--- TASK38/Program.cs
+++ TASK38/Program.cs
@@ -4,7 +4,7 @@
 
 double[] CreateArrayRndDouble(int size, int min, int max) //создание массива с дробными числами
 {
-    double[] arr = new int[size];
+    double[] arr = new double[size];
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
@@ -18,8 +18,45 @@ void PrintArrayDouble(double[] arr) // вывод массива с отсече
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F1}; ");
         else Console.Write($"{arr[i]:F1}"); // F1 - означает округление числа но 2 знаков после запятой
     }
     Console.WriteLine("]");
 }
+
+double MinNumberArray (double[] arr)
+{
+    double minNumber = arr[0];
+    for (int i = 0; i <arr.Length; i++)
+    {
+        if (minNumber > arr[i]) minNumber = arr[i];
+    }
+    return minNumber;
+}
+double MaxNumberArray (double[] arr)
+{
+    double maxNumber = arr[0];
+    for (int i = 0; i <arr.Length; i++)
+    {
+        if (maxNumber < arr[i]) maxNumber = arr[i];
+    }
+    return maxNumber;
+}
+void DifferenceMaxMin (double max, double min)
+{
+    double difference = max - min;
+    Console.WriteLine($"Разница между максимальным и минимальным элементов массива - > {difference:F1}");
+}
+
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+double[] createArrayRndDouble = CreateArrayRndDouble(sizeArray, 0, 100);
+PrintArrayDouble(createArrayRndDouble);
+
+double minNumberArray = MinNumberArray(createArrayRndDouble);
+double maxNumberArray = MaxNumberArray(createArrayRndDouble);
+Console.WriteLine($"Максимальное число: {maxNumberArray:F1}; минимальное число: {minNumberArray:F1}");
+DifferenceMaxMin(maxNumberArray, minNumberArray);
+
+
