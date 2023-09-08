ef7c33a3fe31a98175ce81afa456fda3a7471ef4
Sergej <serega95kost@gmail.com>
2023-09-08 13:48:33 +0300

    Задача 53

--- TASK50/Program.cs
+++ TASK50/Program.cs
@@ -6,3 +6,62 @@
 // 5 9 2 3
 // 8 4 2 4
 // 1,7 -> такого элемента в массиве нет
+
+
+int[,] CreateMatrixRndInt(int rows, int colunns, int min, int max)
+{
+    int[,] matrix = new int[rows, colunns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],6}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool IndexInTheMatrix(int[,] matr, int lin, int colum)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+        }
+    } 
+    return true;
+
+
+}
+
+void PrintNumberIndex(int[,] matrix, int lin, int colum)
+{
+    Console.WriteLine($"Число находящееся на позиции {lin},{colum} -> {matrix[lin, colum]}");
+}
+
+Console.WriteLine("Введите первый индекс(строки): ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первый индекс(столбца): ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] createMatrixRndInt = CreateMatrixRndInt(3, 4, -100, 100);
+PrintArray(createMatrixRndInt);
+Console.WriteLine();
+bool indexInTheMatrix = IndexInTheMatrix(createMatrixRndInt, line, column);
+Console.WriteLine(indexInTheMatrix ? $"ДА" : "такого элемента в массиве нет");
+PrintNumberIndex(createMatrixRndInt, line, column);
--- TASK52/Program.cs
+++ TASK52/Program.cs
@@ -1,8 +1,66 @@
 ﻿// Задайте двумерный массив из целых чисел.
 // Найдите среднее арифметическое элементов в каждом столбце.
-
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int colunns, int min, int max)
+{
+    int[,] matrix = new int[rows, colunns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j],6}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[] ArithmeticMean(int[,] matr)
+{
+    double sum = 0;
+    double[] arr = new double[matr.GetLength(1)];
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            arr[j] += matr[i, j] / matr.GetLength(0);
+            arr[j] = Math.Round(arr[j], 1, MidpointRounding.ToZero);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]} |");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[,] createMatrixRndInt = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(createMatrixRndInt);
+Console.WriteLine();
+double[] arithmeticMean = ArithmeticMean(createMatrixRndInt);
+PrintArray(arithmeticMean);
